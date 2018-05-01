cfad2d22762b2691b112c568023bf3aa9d2cab10
KENI TK <tkkeni984152245@gmail.com>
2018-05-01 20:55:48 +0800

    l

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import psutil
 import json
 import os
 
+print(time.localtime())
 '''
 def load_avg():
     f = os.popen("uptime")
@@ -12,26 +13,10 @@ def load_avg():
     return (a[a.find("average:") + 8:]).strip()
 
 
-timeCount = 0
-interval_speed = 1
-interval_other = 10
 JSON = {}
-
+log = {}
 while True:
     JSON.clear()
-    network = psutil.net_io_counters(False)
-    time.sleep(1)
-    network2 = psutil.net_io_counters(False)
-
-    JSON['up'] = (network2.bytes_sent - network.bytes_sent) / 1024
-    JSON['down'] = (network2.bytes_recv - network.bytes_recv) / 1024
-    with open("/usr/share/nginx/html/speed.json", 'w+') as speed:
-        speed.write("CallBack(" + json.dumps(JSON) + ")")
-        speed.close()
-    timeCount += 1
-    if timeCount < 10:
-        continue
-    timeCount = 0
     mem = psutil.virtual_memory()
     JSON.clear()
     JSON['CPU'] = str(psutil.cpu_percent(1))
@@ -57,16 +42,27 @@ while True:
     JSON['network'] = {}
     JSON['network']['used'] = (int(vnstat['interfaces'][0]['traffic']['total']['rx']) + int(vnstat['interfaces'][0]['traffic']['total']['tx'])) / 1024
     JSON['network']['total'] = 1000
+    network = psutil.net_io_counters(False)
+    time.sleep(1)
+    network2 = psutil.net_io_counters(False)
+
+    JSON['network']['up'] = (network2.bytes_sent - network.bytes_sent) / 1024
+    JSON['network']['down'] = (network2.bytes_recv - network.bytes_recv) / 1024
+
     #单位为KB
-    JSON['time'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    Time = time.localtime()
+    JSON['time'] = time.strftime("%Y-%m-%d %H:%M:%S", Time)
     with open("/usr/share/nginx/html/info.json", 'w+') as speed:
         speed.write("CallBack(" + json.dumps(JSON) + ")")
         speed.close()
     print(JSON)
-
+    
+    if (time_)[4] % 5 == 0:
+        
+        with open("./" + time.strftime("%m-%d %H:%M:%S",time.localtime() + ".json"),"w+") as file:
+            print(file.read())
+            print(file.write("12d4as85d4as6d4as65das"))
     print("done")
+    time.sleep(9)
+
 '''
-with open("./Test.json","w+") as file:
-    print(file.read())
-    print(file.write("12d4as85d4as6d4as65das"))
-print (time.strftime("%Y-%m-%d", time.localtime()))
